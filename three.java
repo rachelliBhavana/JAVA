20bb1a786bd6165e13739abe1679c3cae6355dd2
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-07-20 18:14:42 +0530

    Add files via upload

---
+++ three.java
@@ -0,0 +1,44 @@
+/*
+Write Java program which accept N numbers from user and display
+all such elements which are even and divisible by 5.
+Input : N : 6
+Elements : 85 66 3 80 93 88
+Output : 80
+*/
+import java.util.*;
+
+class Arraydemo
+{
+	public void display(int arr[])
+	{
+		int icnt1=0;
+		System.out.println();
+		for(int i=0;i<arr.length;i++)
+		{
+			if((arr[i]%5==0)&&(arr[i]%2==0))
+			{
+				System.out.println(arr[i]);
+			}
+		}
+		
+	}
+}
+
+class three
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj=new Scanner(System.in);
+		System.out.println("enter no of elements");
+		int size=sobj.nextInt();
+		int arr[]=new int [size];
+		System.out.println("enter numbers");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sobj.nextInt();
+		}
+		Arraydemo obj=new Arraydemo();
+		obj.display(arr);
+		
+	}
+}
