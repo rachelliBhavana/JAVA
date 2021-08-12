d2cd221627b40fc299a95f1ecd0fffa4068594ac
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-08-13 00:07:25 +0530

    Add files via upload

---
+++ p216.java
@@ -0,0 +1,69 @@
+import java.util.*;
+class Matrix
+{
+	public int Arr[][];
+	public Matrix(int irow,int icol)
+	{
+		Arr=new int[irow][icol];
+	}
+	protected void finalize()
+	{
+		Arr=null;
+	}
+	public void accept()
+	{
+		Scanner sobj=new Scanner(System.in);
+		System.out.println("enter the elements");
+		for(int i=0;i<Arr.length;i++)
+		{
+			for(int j=0;j<Arr[i].length;j++)
+			{
+				Arr[i][j]=sobj.nextInt();
+			}
+		}
+	}
+	public void display()
+	{
+		System.out.println("enter the elements are");
+		for(int i=0;i<Arr.length;i++)
+		{
+			for(int j=0;j<Arr[i].length;j++)
+			{
+				System.out.print(Arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	public int addition()
+	{
+        int isum=0;
+		for(int i=0;i<Arr.length;i++)
+		{
+			for(int j=0;j<Arr[i].length;j++)
+			{
+				isum=isum+Arr[i][j];
+			}
+		}
+		return isum;
+	}
+}
+class p216
+{
+	public static void  main(String str[])
+	{
+		Scanner sobj=new Scanner(System.in);
+		System.out.println("enter number of rows");
+		int row=sobj.nextInt();
+
+		System.out.println("enter number of columns");
+		int col=sobj.nextInt();
+
+		Matrix mobj=new Matrix(row,col);
+		mobj.accept();
+		mobj.display();
+		int ret=mobj.addition();
+		System.out.println("addition is "+ret);
+		
+		System.gc();
+	}
+}
